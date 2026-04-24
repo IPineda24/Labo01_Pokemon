0aa955ca712701c4da9f0d9f3ff601b71311de8c
Irvin Pineda <00024420@uca.edu.sv>
2026-04-24 10:29:43 -0600

    Start project :rocket:

---
+++ src/main/java/org/example/pokemonlabo1/PokemonLabo1Application.java
@@ -0,0 +1,13 @@
+package org.example.pokemonlabo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonLabo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonLabo1Application.class, args);
+    }
+
+}
